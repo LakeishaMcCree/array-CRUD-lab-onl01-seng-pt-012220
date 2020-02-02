c1874a4aff443bc7869b77b9b92a189d207bce14
Lakeisha McCree <lmccree215@icloud.com>
2020-02-02 17:48:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   ["black", "white", "pink", "blue"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array,element)
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
